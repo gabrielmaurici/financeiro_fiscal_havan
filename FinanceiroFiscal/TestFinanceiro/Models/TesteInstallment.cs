c6860f867c2055993a287d587ea6bc143c456941
lorenzo <lomichelotti@hotmail.com>
2021-08-26 11:32:12 -0300

    Feito os testes

---
+++ FinanceiroFiscal/TestFinanceiro/Models/TesteInstallment.cs
@@ -0,0 +1,36 @@
+﻿using Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace TestFinanceiro.Models
+{
+    public class TesteInstallment
+    {
+        [Fact]
+        public void TesteVariaveisInstallment()
+        {
+            int i = 10;
+            float f = 1.0f;
+            var ii = typeof(int);
+            var ff = typeof(float);
+            Installment installment = Activator.CreateInstance<Installment>();
+
+            installment.ParcelQuantity = i;
+            installment.Id = i;
+            installment.TaxFees = f;
+
+            //teste tipo
+            Assert.IsAssignableFrom(ii, installment.ParcelQuantity);
+            Assert.IsAssignableFrom(ii, installment.Id);
+            Assert.IsAssignableFrom(ff, installment.TaxFees);
+            //teste assign
+            Assert.NotEqual(0, installment.Id);
+            Assert.NotEqual(0, installment.ParcelQuantity);
+            Assert.NotEqual(0, installment.TaxFees);
+        }
+    }
+}
