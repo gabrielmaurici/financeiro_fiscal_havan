48e25f015b695fd25d974b8957c6f2af3c70fd3d
lorenzo <lomichelotti@hotmail.com>
2021-08-26 11:16:07 -0300

    Create model installment

---
+++ FinanceiroFiscal/Data/Models/Installment.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Models
+{
+    class Installment : BaseModel
+    {
+        public int ParcelQuantity { get; set; }
+        public float TaxFees { get; set; }
+    }
+}
