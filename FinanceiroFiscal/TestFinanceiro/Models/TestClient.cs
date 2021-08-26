d01a477417d0c972ca8d0aa85888cb46f783f504
lorenzo <lomichelotti@hotmail.com>
2021-08-26 16:11:02 -0300

    Update Client Model Test

--- FinanceiroFiscal/TestFinanceiro/Models/TestClient.cs
+++ FinanceiroFiscal/TestFinanceiro/Models/TestClient.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using Xunit;
@@ -29,41 +30,17 @@ namespace TestFinanceiro.Models
             Assert.IsType<Client>(c);
         }
         [Fact]
-        public void TestClientProperties()
+        public void TesteClientPropertiesExist()
         {
-            Client c;
-            string setString = "a";
-            DateTime d = new DateTime(1999, 08, 22);
-            var st = typeof(string);
-            var dt = typeof(DateTime);
+            Type clientType = typeof(Client);
+            PropertyInfo pI = clientType.GetProperties().FirstOrDefault(p => p.Name == "Name");
 
-            c = Activator.CreateInstance<Client>();
-            c.Name = setString;
-            c.Cpf = setString;
-            c.Rg = setString;
-            c.Birthday = d;
-
-            //var assignName = c.Name;
-            //var assignCpf = c.Cpf;
-            //var assignRg = c.Rg;
-            //var assignBirthday = c.Birthday;
-
-            //TypeTestSet
-            Assert.IsAssignableFrom( st, c.Name);
-            Assert.IsAssignableFrom( st, c.Cpf);
-            Assert.IsAssignableFrom( st, c.Rg);
-            Assert.IsAssignableFrom( dt, c.Birthday);
-            //AssignTestSet
-            Assert.NotEqual<string>("", c.Name);
-            Assert.NotEqual<string>("", c.Cpf);
-            Assert.NotEqual<string>("", c.Rg);
-            Assert.Equal<DateTime>(d, c.Birthday);
-            //TypeTestGet
-            //Assert.IsAssignableFrom(c.Name.GetType(), assignName);
-            //Assert.IsAssignableFrom(c.Cpf.GetType(), assignCpf);
-            //Assert.IsAssignableFrom(c.Rg.GetType(), assignRg);
-            //Assert.IsAssignableFrom(c.Birthday.GetType(), assignBirthday);
-            //AssignTestGet
+            pI = clientType.GetProperties().FirstOrDefault(p => p.Name == "Birthday");
+            Assert.NotNull(pI);
+            pI = clientType.GetProperties().FirstOrDefault(p => p.Name == "Cpf");
+            Assert.NotNull(pI);
+            pI = clientType.GetProperties().FirstOrDefault(p => p.Name == "Rg");
+            Assert.NotNull(pI);
         }
     }
 }
