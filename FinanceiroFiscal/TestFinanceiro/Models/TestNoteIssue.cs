f043ee8b83b7a8094fce7f59c14e58abf5fd6b60
rafinha-96 <rafa10_zc@hotmail.com>
2021-08-26 15:02:16 -0300

    Create NoteIssue With Test

---
+++ FinanceiroFiscal/TestFinanceiro/Models/TestNoteIssue.cs
@@ -0,0 +1,48 @@
+﻿using Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace TestFinanceiro.Models
+{
+    public class TestNoteIssue
+    {
+        [Fact]
+        public void TestInhertForBase()
+        {
+            NoteIssue Note = Activator.CreateInstance<NoteIssue>();
+            Assert.IsAssignableFrom<BaseModel>(Note);
+        }
+        [Fact]
+        public void TestInstanceNoteIssue()
+        {
+            NoteIssue Instance;
+            Instance = Activator.CreateInstance<NoteIssue>();
+            Assert.IsType<NoteIssue>(Instance);
+        }
+        [Fact]
+        public void TestPropertys()
+        {
+            NoteIssue Instance;
+            Instance = Activator.CreateInstance<NoteIssue>();
+
+            decimal testdecimal = 10.0m;
+            int testint = 10;
+
+            Type i = typeof(int);
+            Type d = typeof(decimal);
+
+            Instance.Valor = testdecimal;
+            Instance.BarCode = testint;
+
+            Assert.IsAssignableFrom(d, Instance.Valor);
+            Assert.IsAssignableFrom(i, Instance.BarCode);
+
+            Assert.Equal(testdecimal, Instance.Valor);
+            Assert.Equal(testint, Instance.BarCode);
+        }
+    }
+}
