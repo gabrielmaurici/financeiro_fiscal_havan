c0675ccadca35f6c3ac498dfb843deaa899b8aa7
vinicius <vinicius_stumpf@edu.univali.br>
2021-08-26 13:01:44 -0300

    Criando model de contato Closed#8

--- FinanceiroFiscal/TestFinanceiro/Models/TestContactModel.cs
+++ FinanceiroFiscal/TestFinanceiro/Models/TestContactModel.cs
@@ -1,9 +1,11 @@
 ﻿using Data;
+using Data.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
 using Xunit;
 
 namespace TestFinanceiro.Models
