76ea4b2c5db0b341b82d892a3605cbd9585c80f5
lorenzo <lomichelotti@hotmail.com>
2021-08-26 16:27:14 -0300

    Update Client Model Test

--- FinanceiroFiscal/TestFinanceiro/Models/TestClient.cs
+++ FinanceiroFiscal/TestFinanceiro/Models/TestClient.cs
@@ -42,5 +42,76 @@ namespace TestFinanceiro.Models
             pI = clientType.GetProperties().FirstOrDefault(p => p.Name == "Rg");
             Assert.NotNull(pI);
         }
+        [Fact]
+        public void TestClientPropertiesType()
+        {
+            Type clientType = typeof(Client);
+
+            PropertyInfo pI = clientType.GetProperties().FirstOrDefault(p => p.Name == "Name");
+            Type typeToCheck = typeof(string);
+            Type typeOfProperty = pI != null ? pI.PropertyType : null;
+            Assert.Equal(typeToCheck, typeOfProperty);
+
+            pI = clientType.GetProperties().FirstOrDefault(p => p.Name == "Cpf");
+            typeToCheck = typeof(string);
+            typeOfProperty = pI != null ? pI.PropertyType : null;
+            Assert.Equal(typeToCheck, typeOfProperty);
+
+            pI = clientType.GetProperties().FirstOrDefault(p => p.Name == "Rg");
+            typeToCheck = typeof(string);
+            typeOfProperty = pI != null ? pI.PropertyType : null;
+            Assert.Equal(typeToCheck, typeOfProperty);
+
+            pI = clientType.GetProperties().FirstOrDefault(p => p.Name == "Birthday");
+            typeToCheck = typeof(DateTime);
+            typeOfProperty = pI != null ? pI.PropertyType : null;
+            Assert.Equal(typeToCheck, typeOfProperty);
+        }
+        [Fact]
+        public void TestarSePropriedadeDescricaoGet()
+        {
+            Client client = new Client();
+            Type type = typeof(Client);
+
+            PropertyInfo pI = type.GetProperties().FirstOrDefault(p => p.Name == "Name");
+            object valueProp = null;
+            if (pI != null && pI.PropertyType == typeof(String))
+            {
+
+                pI.SetValue(client, "");
+                valueProp = pI.GetValue(client);
+            }
+            Assert.NotNull(valueProp);
+
+            pI = type.GetProperties().FirstOrDefault(p => p.Name == "Cpf");
+            valueProp = null;
+            if (pI != null && pI.PropertyType == typeof(String))
+            {
+
+                pI.SetValue(client, "");
+                valueProp = pI.GetValue(client);
+            }
+            Assert.NotNull(valueProp);
+
+            pI = type.GetProperties().FirstOrDefault(p => p.Name == "Rg");
+            valueProp = null;
+            if (pI != null && pI.PropertyType == typeof(String))
+            {
+
+                pI.SetValue(client, "");
+                valueProp = pI.GetValue(client);
+            }
+            Assert.NotNull(valueProp);
+
+            pI = type.GetProperties().FirstOrDefault(p => p.Name == "Birthday");
+            valueProp = null;
+            if (pI != null && pI.PropertyType == typeof(DateTime))
+            {
+
+                pI.SetValue(client, new DateTime(1999,02,22));
+                valueProp = pI.GetValue(client);
+            }
+            Assert.NotNull(valueProp);
+        }
     }
 }
