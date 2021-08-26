8095cb131cf05da26bd5124bb4e6ca2614a84a5e
vinicius <vinicius_stumpf@edu.univali.br>
2021-08-26 12:48:12 -0300

    Criando model de contato

---
+++ FinanceiroFiscal/Data/ContactModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data
+{
+    public class ContactModel:BaseModel
+    {
+        public string Phone { get; set; }
+        public string email { get; set; }
+    }
+}
