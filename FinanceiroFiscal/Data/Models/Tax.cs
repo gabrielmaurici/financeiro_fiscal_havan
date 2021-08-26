cebf549b9fde0b8b5728bc1bf59f4ea29e21f4e1
leonardocascaes <leonardo.cascaes@hotmail.com>
2021-08-26 13:05:55 -0300

    Create model and test tax

---
+++ FinanceiroFiscal/Data/Models/Tax.cs
@@ -0,0 +1,8 @@
+﻿namespace Data.Models
+{
+    public class Tax : BaseModel
+    {
+        public string Name { get; set; }
+        public float TaxValue { get; set; }
+    }
+}
---
+++ FinanceiroFiscal/TestFinanceiro/Models/TestTax.cs
@@ -0,0 +1,43 @@
+﻿using Data.Models;
+using Xunit;
+
+namespace TestFinanceiro.Models
+{
+    public class TestTax
+    {
+        [Fact]
+        public void TestHerancaBaseTaxModel()
+        {
+            //Arrange
+            Tax tax = new Tax();
+            //Act
+            //Assert
+            Assert.IsAssignableFrom<BaseModel>(tax);
+        }
+
+        [Fact]
+        public void TestPropNameTaxModel()
+        {
+            //Arrange
+            Tax tax = new Tax();
+            string expectedValue = "Ok";
+            //Act
+            tax.Name = expectedValue;
+            //Assert
+            Assert.Equal(expectedValue, tax.Name);
+            Assert.IsType<string>(tax.Name);
+        }
+        [Fact]
+        public void TestPropTaxValueTaxModel()
+        {
+            //Arrange
+            Tax tax = new Tax();
+            float expectedValue = 15.35F;
+            //Act
+            tax.TaxValue = expectedValue;
+            //Assert
+            Assert.Equal(expectedValue, tax.TaxValue);
+            Assert.IsType<float>(expectedValue);
+        }
+    }
+}
