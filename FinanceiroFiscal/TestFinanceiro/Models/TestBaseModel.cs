14f9165716c6ce99c8e176c8889af761d0f2af59
leonardocascaes <leonardo.cascaes@hotmail.com>
2021-08-26 11:00:13 -0300

    Criado Data com Model Base + Test De Instanciação

---
+++ FinanceiroFiscal/TestFinanceiro/Models/TestBaseModel.cs
@@ -0,0 +1,24 @@
+﻿using Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace TestFinanceiro.Models
+{
+    public class TestBaseModel
+    {
+        [Fact]
+        public void TestInstanciacaoBaseModel()
+        {
+            //Arrange
+            Action tb;
+            //Act
+            tb = () => Activator.CreateInstance<BaseModel>();
+            //Assert
+            Assert.Throws<MissingMethodException>(tb);
+        }
+    }
+}
